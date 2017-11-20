3033662e26d11e1092800b215bea0ab3ec0bc8c9
vitor25 <vitor.araujo@ufrj.br>
2017-11-20 19:50:49 -0200

    adding phone generation to passenger seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
+        
+        $faker = Faker::create('en_US');
+
+        app('db')->statement('SET FOREIGN_KEY_CHECKS = 0;');
+        app('db')->statement('truncate table passageiro;');
+        app('db')->statement('truncate table telefone;');
+        app('db')->statement('SET FOREIGN_KEY_CHECKS = 1;');
 
         for ($i=0; $i < 10; $i++) { 
-        	app('db')->insert('insert into passageiro values ('."\"".strval($faker->randomNumber($nbDigits = 9, $strict = true))."\",\"".$faker->name."\");");
+        	$nome = $faker->name;
+        	$cpf = strval($faker->randomNumber($nbDigits = 9, $strict = true));
+        	$phone_count = random_int(1, 2);
+        	$phones = [];
+        	for ($j=0; $j < $phone_count; $j++) { 
+        		array_push($phones, $faker->phoneNumber);
+        	}
+
+        	app('db')->insert('insert into passageiro values ('."\"".$cpf."\",\"".$nome."\");");
+        	foreach ($phones as $numero) {
+        		app('db')->insert('insert into telefone values('."\"".$cpf."\",\"".$numero."\");");
+        	}
         }
     }
 }
